d8480dde4f5af3d3f08d80e23b6f2f782b346dfd
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-04-19 17:23:59 +0500

    ignore firbase folder

--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -39,20 +39,21 @@ const Register = () => {
             <div className='bg-conic from-blue-600 to-sky-400 to-50% h-full'>
                 <h2 className='font-extrabold text-center text-3xl text-primary underline underline-offset-8 pt-12'>Enter Registeration Details</h2>
                 <div className="mt-4 flex items-center justify-between mx-5">
-                    <div className='p-4 border border-secondary rounded-xl w-[70%] h-full bg-no-repeat bg-center bg-cover my-12 text-center mx-auto' style={{ backgroundImage: `url(${loginImg})` }}>
+                    <div className='p-6 border border-secondary rounded-xl w-[70%] bg-no-repeat bg-bottom-right bg-cover my-12 text-center mx-auto' style={{ backgroundImage: `url(${loginImg})` }}>
                         <div className='flex flex-col justify-center space-y-12 h-full mx-auto w-1/2'>
-                            <input className='border-b-2 border-gray-100 outline-0' placeholder='Name' type='text' />
-                            <input className='border-b-2 border-gray-100 outline-0' placeholder='Email' type='email' />
-                            <input className='border-b-2 border-gray-100 outline-0' placeholder='Password' type='password' />
+                            <input className='inputPlaceholder border-b-2 border-gray-300 outline-0' placeholder='Name' type='text' />
+                            <input className='inputPlaceholder border-b-2 border-gray-300 outline-0' placeholder='Email' type='email' />
+                            <input className='inputPlaceholder border-b-2 border-gray-300 outline-0' placeholder='Password' type='password' />
+                            <input className='inputPlaceholder border-b-2 border-gray-300 outline-0' placeholder='Confirm Password' type='password' />
                             <div className='flex space-x-5'>
-                                <p className='text-[#907974]'>Gender</p>
+                                <p className='text-[#332725]'>Gender</p>
                                 <input placeholder='male' className='border-b-2 border-gray-100 outline-0' type='radio' value="Male" checked={radioBtn === 'Male'} onChange={(e) => toggleRadioBtn(e.target.value)} />
-                                <label className='text-[#907974]' for="male">Male</label>
+                                <label className='text-[#332725]' for="male">Male</label>
                                 <input placeholder='female' className='border-b-2 border-gray-100 outline-0' type='radio' checked={radioBtn === 'Female'} value="Female" onChange={(e) => toggleRadioBtn(e.target.value)} />
-                                <label className='text-[#907974]' for="female">Female</label>
+                                <label className='text-[#332725]' for="female">Female</label>
                             </div>
-                            <div className='flex space-x-4'>
-                                <label className="cursor-pointer font-normal text-[#907974] rounded-md p-4">
+                            <div className='flex items-center space-x-4'>
+                                <label className="cursor-pointer font-normal text-[#332725] rounded-md p-4">
                                     Upload Image
                                 </label>
                                 <input
@@ -72,11 +73,11 @@ const Register = () => {
                                     )}
                                     {
                                         selectedImage &&
-                                        <p onClick={changeImgOption} className='text-[#907974] cursor-pointer'>
-                                        Change Image
-                                    </p>
+                                        <p onClick={changeImgOption} className='text-[#332725] cursor-pointer underline font-medium'>
+                                            Change Image
+                                        </p>
                                     }
-                                   
+
                                 </div>
                             </div>
                             <button onClick={registerRoute} className='bg-primary w-1/2 mx-auto text-white px-5 py-2 rounded-full cursor-pointer'>Register</button>
