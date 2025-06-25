e2ad4be66a9085192757ecc48e6659d63cd9d335
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-25 15:39:58 +0500

    new changess

--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,26 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const express = require('express');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = 5000;
 
-// Use default middlewares (logger, CORS, etc.)
+// Middleware
 server.use(middlewares);
 
-// Add a custom route for "/"
+// Custom route for '/'
 server.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello from the root route!');
 });
 
-// Optional rewriter for custom routes
-server.use(
-    jsonServer.rewriter({
-        "/*": "/$1",
-    })
-);
+// Rewriter (optional)
+server.use(jsonServer.rewriter({
+    '/*': '/$1',
+}));
 
-// Use default JSON Server router
+// JSON Server routes (/users, etc.)
 server.use(router);
 
 // Start server
 server.listen(port, () => {
     console.log(`JSON Server is running at http://localhost:${port}`);
 });
-
-// Export the Server API
-module.exports = server;
