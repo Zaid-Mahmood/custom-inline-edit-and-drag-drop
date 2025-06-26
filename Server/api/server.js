f24b9d4f2ecd1c664484778680c925ae29970762
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 21:11:06 +0500

    new group added

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,12 +1,11 @@
 // server.js
-// See https://github.com/typicode/json-server#module
 const fs = require('fs')
 const path = require('path')
-const jsonServer = require('json-server') // <--- Move this line here!
-const express = require('express') // If you added this for static files
+const jsonServer = require('json-server')
+const express = require('express') // <--- Make sure this is added
 
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db) // Now `jsonServer` is defined
+const router = jsonServer.router(db)
 
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
@@ -14,18 +13,20 @@ const port = process.env.PORT || 5000;
 
 server.use(middlewares)
 
-// Serve static files from the 'public' directory (if you're using this)
-// server.use(express.static(path.join(__dirname, 'public')))
+// Serve static files from the 'public' directory
+// Ensure 'public' folder is at your project root, parallel to 'api'
+server.use(express.static(path.join(__dirname, '..', 'public'))) // <--- Adjusted path!
+                                                               // If server.js is in /api, then public is ../public from __dirname
 
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
- '/api/*': '/$1',
- '/blog/:resource/:id/show': '/:resource/:id'
+'/api/*': '/$1',
+'/blog/:resource/:id/show': '/:resource/:id'
 }))
 
 server.use(router)
 server.listen(port, () => {
- console.log('JSON Server is running')
+console.log('JSON Server is running')
 })
 
 // Export the Server API
