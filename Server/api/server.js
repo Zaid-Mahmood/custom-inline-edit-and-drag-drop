48326a398ffd1da3e902aa19b196ab89b0e7d97f
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 16:11:15 +0500

    change in server again

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,43 +1,19 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-// Create server
 const server = jsonServer.create()
-
-// Set up default middlewares (logger, static, cors, etc.)
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
 server.use(middlewares)
-
-// Set up lowdb with FileSync adapter
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
-// Set default structure if db is empty
-db.defaults({  users: [], loginUser: [] }).write()
-
-// Use default router with lowdb
-const router = jsonServer.router(db)
-
-// Optional: Custom routes before router
-server.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the JSON Server with lowdb@1.0.0' })
-})
-
-// Optional: URL rewrites
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-// Mount router
 server.use(router)
-
-// Start the server
-const PORT = process.env.PORT || 5000
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
 
-// Export server for testing or deployment
+// Export the Server API
 module.exports = server
