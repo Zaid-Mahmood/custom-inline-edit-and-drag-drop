4399c5004f1b08f9a61eba0eb5fa1b4e31750c60
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 20:04:39 +0500

    server changed

--- Server/api/server.js
+++ Server/api/server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+const port = process.env.PORT || 5000
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
@@ -11,7 +11,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(5000, () => {
+server.listen(port, () => {
     console.log('JSON Server is running')
 })
 
