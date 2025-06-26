d14fa51bcdb2f430fe31d59124745f1b6fb72812
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 20:46:23 +0500

    port updated

--- Server/api/server.js
+++ Server/api/server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
