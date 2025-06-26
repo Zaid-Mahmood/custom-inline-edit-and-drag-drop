7e83f0903bf5eba2ebcef5b1918c02e3a9f8f1be
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 20:50:11 +0500

    new port added

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,7 +1,9 @@
 // See https://github.com/typicode/json-server#module
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 5000;
 server.use(middlewares)
