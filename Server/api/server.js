61565a23f2dc5900c9268d07489f34eb469f2c21
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 21:44:48 +0500

    .env updated added

--- Server/api/server.js
+++ Server/api/server.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 const jsonServer = require('json-server')
-const express = require('express') // <--- Make sure this is added
+const express = require('express')
 
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
 const router = jsonServer.router(db)
@@ -13,21 +13,17 @@ const port = process.env.PORT || 5000;
 
 server.use(middlewares)
 
-// Serve static files from the 'public' directory
-// Ensure 'public' folder is at your project root, parallel to 'api'
-server.use(express.static(path.join(__dirname, '..', 'public'))) // <--- Adjusted path!
-                                                               // If server.js is in /api, then public is ../public from __dirname
 
-// Add this before server.use(router)
+server.use(express.static(path.join(__dirname, '..', 'public')))
+
 server.use(jsonServer.rewriter({
-'/api/*': '/$1',
-'/blog/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 
 server.use(router)
 server.listen(port, () => {
-console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
