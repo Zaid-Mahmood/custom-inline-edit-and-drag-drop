b5e3cf0aa48d9a9194c4e61ce84caa3f7a08eaf6
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-26 20:58:07 +0500

    server chandesss

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,23 +1,29 @@
-// See https://github.com/typicode/json-server#module
+// server.js
 const fs = require('fs')
-const path = require('path') // <--- Add this line
-const jsonServer = require('json-server') // Moved up for better organization
+const path = require('path')
+const jsonServer = require('json-server')
+const express = require('express') // <--- Add this
 
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db)
+// ... (rest of your existing setup)
+
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 5000;
 
 server.use(middlewares)
+
+// Serve static files from the 'public' directory
+server.use(express.static(path.join(__dirname, 'public'))) // <--- Add this line BEFORE server.use(router)
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
- '/api/*': '/$1',
- '/blog/:resource/:id/show': '/:resource/:id'
+'/api/*': '/$1',
+'/blog/:resource/:id/show': '/:resource/:id'
 }))
+
 server.use(router)
 server.listen(port, () => {
-console.log('JSON Server is running')
+ console.log('JSON Server is running')
 })
 
 // Export the Server API
