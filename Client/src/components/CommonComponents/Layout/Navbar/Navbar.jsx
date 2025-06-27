18039056513971e3f3eaf7c2169cfee02188a6cd
Zaid-Mahmood <mahmoodzaid603@gmail.com>
2025-06-27 11:33:11 +0500

    del api added

--- Client/src/components/CommonComponents/Layout/Navbar/Navbar.jsx
+++ Client/src/components/CommonComponents/Layout/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = ({ showSidebar }) => {
     dispatch(setShowSideBar());
   }
   const delLoggedUser = () => {
-    useDeleteUser(`${postUrl}/${loginCredentials?.id}`)
+    useDeleteUser(`https://custom-inline-edit-and-drag-drop.vercel.app/${loginCredentials?.id}` || `${postUrl}/${loginCredentials?.id}`)
     dispatch(setSuccessMode({ show: true, type: 'logout' }))
     dispatch(setLoginCrederntials(null))
   }
@@ -49,52 +49,52 @@ const Navbar = ({ showSidebar }) => {
 
   return (
     <>
-    <div className={`fixed top-0 z-20  ${widthClass}`}>
-      <div className='relative'>
-        <img className='xs:w-12 xs:h-12 sm:w-44 sm:h-32 xs:left-5 md:left-12 absolute' src={logo} alt="calm-logo" />
+      <div className={`fixed top-0 z-20  ${widthClass}`}>
+        <div className='relative'>
+          <img className='xs:w-12 xs:h-12 sm:w-44 sm:h-32 xs:left-5 md:left-12 absolute' src={logo} alt="calm-logo" />
 
-        <div className="flex justify-between xs:w-[70%] sm:w-[88%] lg:w-full" style={{ backgroundColor: headers[layoutId ?? localStorageLayoutId].bgColor }}>
-          <div className='flex items-center md:hidden xs:ml-[45%] sm:ml-[50%]' >
-            <GiHamburgerMenu onClick={openMenu} className='text-white' />
-          </div>
-          <div>
-            {headers[layoutId ?? localStorageLayoutId].component}
-          </div>
-          <div>
-            {NavBtns.map((item, id) => (
-              <div className='flex' key={id}>
-                <div className='xs:hidden md:flex items-center'>
-                  {socialMediaIcons.map((item, id) => (
-                    <p className='mx-2' key={id}>{item}</p>
-                  ))}
-                </div>
-                <div className='ml-4'>
-                  <button onClick={showSideMenuFunction} className='bg-[#604058] text-white xs:ml-0 sm:ml-12 sm:p-4 xs:p-3 float-end cursor-pointer xs:text-xs text-lg'>{item.editBtn}</button>
-                  <div className='flex justify-end items-center gap-x-24 sm:gap-x-12'>
-                    <img className='xs:w-10 xs:h-10 rounded-full object-contain mt-1' src={item.img} alt='profile-img' />
+          <div className="flex justify-between xs:w-[70%] sm:w-[88%] lg:w-full" style={{ backgroundColor: headers[layoutId ?? localStorageLayoutId].bgColor }}>
+            <div className='flex items-center md:hidden xs:ml-[45%] sm:ml-[50%]' >
+              <GiHamburgerMenu onClick={openMenu} className='text-white' />
+            </div>
+            <div>
+              {headers[layoutId ?? localStorageLayoutId].component}
+            </div>
+            <div>
+              {NavBtns.map((item, id) => (
+                <div className='flex' key={id}>
+                  <div className='xs:hidden md:flex items-center'>
+                    {socialMediaIcons.map((item, id) => (
+                      <p className='mx-2' key={id}>{item}</p>
+                    ))}
+                  </div>
+                  <div className='ml-4'>
+                    <button onClick={showSideMenuFunction} className='bg-[#604058] text-white xs:ml-0 sm:ml-12 sm:p-4 xs:p-3 float-end cursor-pointer xs:text-xs text-lg'>{item.editBtn}</button>
+                    <div className='flex justify-end items-center gap-x-24 sm:gap-x-12'>
+                      <img className='xs:w-10 xs:h-10 rounded-full object-contain mt-1' src={item.img} alt='profile-img' />
 
-                    <p className='text-xs mr-4 sm:mr-0  md:text-sm lg:text-lg text-white'>{`Welcome ${getFirstName[0]}!`}</p>
-                    <p className='xs:hidden md:block'>{item.mobileIcon}</p>
-                    <button onClick={logoutUser} className='cursor-pointer xs:hidden md:block'>{item.logoutBtn}</button>
+                      <p className='text-xs mr-4 sm:mr-0  md:text-sm lg:text-lg text-white'>{`Welcome ${getFirstName[0]}!`}</p>
+                      <p className='xs:hidden md:block'>{item.mobileIcon}</p>
+                      <button onClick={logoutUser} className='cursor-pointer xs:hidden md:block'>{item.logoutBtn}</button>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ))}
+            </div>
+          </div>
+          <div className='flex justify-end my-5'>
+            {navItems.map((item, id) => (
+              <ul key={id}>
+                <li className={`mx-4 font-normal ${id === 0 ? 'text-primary' : 'text-black'}`}>{item}</li>
+              </ul>
             ))}
           </div>
-        </div>
-        <div className='flex justify-end my-5'>
-          {navItems.map((item, id) => (
-            <ul key={id}>
-              <li className={`mx-4 font-normal ${id === 0 ? 'text-primary' : 'text-black'}`}>{item}</li>
-            </ul>
-          ))}
-        </div>
-        <div className='xs:ml-[20%] sm:ml-[50%] md:hidden flex items-center'>
-          <SmallScreenNavbar openListMenu={openListMenu} />
+          <div className='xs:ml-[20%] sm:ml-[50%] md:hidden flex items-center'>
+            <SmallScreenNavbar openListMenu={openListMenu} />
+          </div>
         </div>
       </div>
-    </div>
-    
+
     </>
   )
 }
